b2f1085be582c9ff54a6e5837a29233644503c0d
Ho Hieu Luc <kingofika123@gmail.com>
2022-04-26 22:18:25 +0700

    add isMongooseModel function

--- server/utils/functions.js
+++ server/utils/functions.js
@@ -2,6 +2,11 @@ const checkIfDuplicateExists = (array) => {
     return new Set(array).size !== array.length;
 };
 
+const isMongooseModel = (obj) => {
+    return obj.constructor.name.toLowerCase() === 'model';
+};
+
 module.exports = {
-    checkIfDuplicateExists
+    checkIfDuplicateExists,
+    isMongooseModel
 };
