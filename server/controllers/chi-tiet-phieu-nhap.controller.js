b00608fb347b008f00feaf8a9b93470fc791e0d3
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-02 16:07:53 +0700

    validate if maSanPhams exist

--- server/controllers/chi-tiet-phieu-nhap.controller.js
+++ server/controllers/chi-tiet-phieu-nhap.controller.js
@@ -1,32 +1,16 @@
 const { UserInputError } = require('apollo-server');
 const mongoose = require('mongoose');
-const PhieuNhap = require('../models/PhieuNhap');
 const chiTietPhieuNhapService = require('../services/chi-tiet-phieu-nhap.service');
+const sanPhamService = require('../services/san-pham.service');
 
 const create = async (idPhieuNhap, chiTietPhieuNhap) => {
     if (!mongoose.isValidObjectId(idPhieuNhap)) {
         throw new UserInputError('Mã phiếu nhập không hợp lệ');
     }
-
     if (!mongoose.isValidObjectId(chiTietPhieuNhap.maSanPham)) {
         throw new UserInputError('Mã sản phẩm không hợp lệ');
     }
-
-    // nếu tìm thấy phiếu nhập và sản phẩm trong phiếu nhập
-    // thì tức là sản phẩm đó đã tồn tại trong phiếu nhập
-    const phieuNhap = await PhieuNhap.findById(idPhieuNhap).populate('chiTiet');
-
-    if (!phieuNhap) {
-        throw new UserInputError('Phiếu nhập không tồn tại');
-    }
-
-    const chiTietPhieuNhapExist = phieuNhap.chiTiet.find(
-        item => item.maSanPham.toString() === chiTietPhieuNhap.maSanPham
-    );
-
-    if (chiTietPhieuNhapExist) {
-        throw new UserInputError('Sản phẩm trong 1 phiếu nhập không được trùng nhau');
-    }
+    await sanPhamService.checkIfExist([chiTietPhieuNhap.maSanPham]);
 
     return chiTietPhieuNhapService.create(idPhieuNhap, chiTietPhieuNhap);
 };
@@ -38,6 +22,8 @@ const update = async (idPhieuNhap, idChiTietPhieuNhap, chiTietPhieuNhap) => {
     if (!mongoose.isValidObjectId(idChiTietPhieuNhap)) {
         throw new UserInputError('Mã chi tiết phiếu nhập không hợp lệ');
     }
+    await sanPhamService.checkIfExist([chiTietPhieuNhap.maSanPham]);
+
     return chiTietPhieuNhapService.update(idPhieuNhap, idChiTietPhieuNhap, chiTietPhieuNhap);
 };
 
