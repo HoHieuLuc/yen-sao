cd0a23a3a486e1c0f896b5b2850dd4d796c04bd7
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-02 23:10:05 +0700

    add plus icon

---
+++ admin/src/components/Utils/Icons/PlusIcon.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+
+const PlusIcon = () => {
+    return (
+        <FontAwesomeIcon icon={faCirclePlus} />
+    );
+};
+
+export default PlusIcon;
