971ccb4dd162561cace025cc3cabfcd4ecbb9423
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-02 23:12:35 +0700

    add custom usePagination hook

--- admin/src/hooks/index.ts
+++ admin/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useDebouncedSearch } from './use-debounced-search';
 export { useDebouncedSearchParams } from './use-debounced-search-params';
+export { usePagination } from './use-pagination';
