538d3b1509128acc97f9910c7995c02367c13dd2
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-02 15:57:20 +0700

    add maPhieuXuat field, move validation to controller

--- server/models/ChiTietPhieuXuat.js
+++ server/models/ChiTietPhieuXuat.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
-const SanPham = require('./SanPham');
 
 const chiTietPhieuXuat = mongoose.Schema(
     {
+        maPhieuXuat: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'PhieuXuat',
+            required: true
+        },
         maSanPham: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'SanPham',
-            required: true,
-            validate: {
-                validator: async (v) => {
-                    const sanPham = await SanPham.findById(v);
-                    return !!sanPham;
-                },
-                message: 'Sản phẩm không tồn tại'
-            }
+            required: true
         },
         soLuongXuat: {
             type: Number,
