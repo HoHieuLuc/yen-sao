717aff0981bab0a01896936163204f54ae0ea5e5
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-04 21:48:28 +0700

    import dayjs

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Provider from './providers/Provider';
+import 'dayjs/locale/vi';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
