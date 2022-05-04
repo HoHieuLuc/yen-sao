605a8a0945d5f5616d388927f18af97a3243919e
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-04 22:02:41 +0700

    add chi tiet phieu nhap form

--- admin/src/components/PhieuNhap/Form/ChiTietPhieuNhapForm.tsx
+++ admin/src/components/PhieuNhap/Form/ChiTietPhieuNhapForm.tsx
@@ -1,55 +1,134 @@
-import { Button, NumberInput, SimpleGrid } from '@mantine/core';
-import { FormList } from '@mantine/form/lib/form-list/form-list';
-import { UseFormReturnType } from '@mantine/form/lib/use-form';
+import { useForm } from '@mantine/form';
+import { useState } from 'react';
+
+import { Button, NumberInput, Select, SimpleGrid } from '@mantine/core';
 import SanPhamSelect from '../../SanPham/Form/SanPhamSelect';
 
-export interface ChiTietPhieuNhapFormData {
-    maSanPham: string;
-    soLuongNhap: number;
-    donGiaNhap: number;
+import { ChiTietPhieuNhapFormData } from '../../../types';
+
+interface BaseProps {
+    type: 'create' | 'update';
+    initialValues?: unknown;
+    onSubmit: (values: ChiTietPhieuNhapFormData) => void;
+    onDelete?: () => void;
+    loading: boolean;
 }
 
-interface Props {
-    phieuNhapForm: UseFormReturnType<{
-        payload: FormList<ChiTietPhieuNhapFormData>;
-    }>
-    index: number;
+interface UpdateProps extends BaseProps {
+    type: 'update';
+    initialValues: {
+        maSanPham: string;
+        tenSanPham: string;
+        soLuongTon: number;
+        soLuongNhap: number;
+        donGiaNhap: number;
+    };
+    onDelete: () => void;
 }
 
-const ChiTietPhieuNhapForm = ({ phieuNhapForm, index }: Props) => {
-    const maSanPham = phieuNhapForm.getListInputProps('payload', index, 'maSanPham');
+interface CreateProps extends BaseProps {
+    type: 'create';
+}
+
+type Props = UpdateProps | CreateProps;
+
+const ChiTietPhieuNhapForm = ({
+    initialValues,
+    type,
+    onSubmit,
+    onDelete,
+    loading
+}: Props) => {
+    const chiTietPhieuNhapForm = useForm<ChiTietPhieuNhapFormData>({
+        initialValues: {
+            maSanPham: type === 'update' ? initialValues.maSanPham : '',
+            soLuongNhap: type === 'update' ? initialValues.soLuongNhap : 0,
+            donGiaNhap: type === 'update' ? initialValues.donGiaNhap : 0,
+        },
+        validate: {
+            maSanPham: (value) => value ? null : 'Vui lòng chọn sản phẩm',
+            soLuongNhap: (value) => value > 0 ? null : 'Số lượng nhập phải lớn hơn 0',
+            donGiaNhap: (value) => value >= 0 ? null : 'Vui lòng nhập đơn giá nhập'
+        }
+    });
+
+    const [lock, setLock] = useState(type === 'update');
+
+    const submit = (values: ChiTietPhieuNhapFormData) => {
+        onSubmit(values);
+    };
 
     return (
-        <SimpleGrid cols={1} spacing='xs'>
-            <SanPhamSelect
-                error={phieuNhapForm.getListInputProps('payload', index, 'maSanPham').error}
-                maSanPham={typeof maSanPham.value === 'string' ? maSanPham.value : ''}
-                setMaSanPham={(maSanPham) =>
-                    phieuNhapForm.getListInputProps('payload', index, 'maSanPham').onChange(maSanPham)
+        <form onSubmit={chiTietPhieuNhapForm.onSubmit(submit)}>
+            <SimpleGrid cols={1} spacing='xs'>
+                {(lock && type === 'update')
+                    ? <Select
+                        label='Tên sản phẩm'
+                        description={`Số lượng tồn: ${initialValues.soLuongTon}`}
+                        defaultValue={initialValues.maSanPham}
+                        data={[{
+                            value: initialValues.maSanPham,
+                            label: initialValues.tenSanPham
+                        }]}
+                        disabled
+                    />
+                    : <SanPhamSelect
+                        error={chiTietPhieuNhapForm.errors.maSanPham}
+                        maSanPham={chiTietPhieuNhapForm.values.maSanPham}
+                        setMaSanPham={(maSanPham) =>
+                            chiTietPhieuNhapForm.setFieldValue('maSanPham', maSanPham)
+                        }
+                    />
                 }
-            />
-            <NumberInput
-                label='Đơn giá nhập'
-                placeholder='Đơn giá nhập'
-                min={0}
-                required
-                {...phieuNhapForm.getListInputProps('payload', index, 'donGiaNhap')}
-            />
-            <NumberInput
-                label='Số lượng nhập'
-                placeholder='Số lượng nhập'
-                min={0}
-                required
-                {...phieuNhapForm.getListInputProps('payload', index, 'soLuongNhap')}
-            />
-            <Button
-                color='red'
-                variant='light'
-                onClick={() => phieuNhapForm.removeListItem('payload', index)}
-            >
-                Xóa
-            </Button>
-        </SimpleGrid>
+                <NumberInput
+                    label='Đơn giá nhập'
+                    placeholder='Đơn giá nhập'
+                    min={0}
+                    {...chiTietPhieuNhapForm.getInputProps('donGiaNhap')}
+                    disabled={lock}
+                />
+                <NumberInput
+                    label='Số lượng nhập'
+                    placeholder='Số lượng nhập'
+                    min={0}
+                    {...chiTietPhieuNhapForm.getInputProps('soLuongNhap')}
+                    disabled={lock}
+                />
+                <SimpleGrid cols={type === 'update' ? 3 : 2}>
+                    {type === 'update' && <>
+                        <Button
+                            color='red'
+                            variant='light'
+                            type='button'
+                            onClick={onDelete}
+                        >
+                            Xóa
+                        </Button>
+                        <Button
+                            type='button'
+                            onClick={() => setLock(prev => !prev)}
+                            color='green'
+                        >
+                            {lock ? 'Sửa' : 'Hủy'}
+                        </Button>
+                    </>}
+                    {type === 'create' && <Button
+                        color='red'
+                        onClick={() => chiTietPhieuNhapForm.reset()}
+                    >
+                        Hủy
+                    </Button>
+                    }
+                    <Button
+                        type='submit'
+                        disabled={lock}
+                        loading={loading}
+                    >
+                        Xác nhận
+                    </Button>
+                </SimpleGrid>
+            </SimpleGrid >
+        </form>
     );
 };
 
