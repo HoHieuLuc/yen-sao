cb15838e527e54f5ab5828ed4c73be7514c7a306
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-05 20:10:45 +0700

    delete functions.ts

--- admin/src/utils/functions.ts
+++
@@ -1,7 +0,0 @@
-export const convertToVND = (money: number) => {
-    return money.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
-};
-
-export const convertToVietnameseDate = (date: Date | number | string) => {
-    return new Date(date).toLocaleString('vi-VN');
-};
