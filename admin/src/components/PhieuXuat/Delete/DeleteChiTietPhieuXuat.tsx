deefa114cce3589654969fe6c3c5d61154c361ff
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-05 20:08:44 +0700

    add delete phieu xuat, chi tiet phieu xuat modals

---
+++ admin/src/components/PhieuXuat/Delete/DeleteChiTietPhieuXuat.tsx
@@ -0,0 +1,59 @@
+import { useMutation } from '@apollo/client';
+
+import { Box, Button, Group, Text } from '@mantine/core';
+
+import { showErrorNotification, showSuccessNotification } from '../../../events';
+import { chiTietPhieuXuatQuery } from '../../../graphql/queries';
+
+interface Props {
+    tenSanPham: string;
+    idPhieuXuat: string;
+    idChiTiet: string;
+    closeModal: () => void;
+}
+
+const DeleteChiTietPhieuXuat = ({ tenSanPham, idPhieuXuat, idChiTiet, closeModal }: Props) => {
+    const [deleteChiTietPhieuXuat, { loading }] = useMutation<
+        never, { idPhieuXuat: string, idChiTiet: string }
+    >(chiTietPhieuXuatQuery.DELETE, {
+        onCompleted: () => {
+            showSuccessNotification('Xóa chi tiết phiếu xuất thành công');
+            closeModal();
+        },
+        onError: (error) => showErrorNotification(error.message)
+    });
+
+    const handleDelete = () => {
+        void deleteChiTietPhieuXuat({
+            variables: {
+                idChiTiet,
+                idPhieuXuat
+            }
+        });
+    };
+
+    return (
+        <Box>
+            <Text>
+                Bạn có chắc muốn xóa sản phẩm <b>{`"${tenSanPham}"`}</b> khỏi phiếu xuất này?
+            </Text>
+            <Group position='right' mt='md'>
+                <Button
+                    color='gray'
+                    onClick={closeModal}
+                >
+                    Hủy
+                </Button>
+                <Button
+                    color='red'
+                    onClick={handleDelete}
+                    loading={loading}
+                >
+                    Xóa
+                </Button>
+            </Group>
+        </Box>
+    );
+};
+
+export default DeleteChiTietPhieuXuat;
