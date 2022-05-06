fdd87e5c8e598601f17483d4abb5f5088e28bdc6
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-06 22:03:43 +0700

    add new icons

---
+++ admin/src/components/Utils/Icons/ArrowUp.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+const ArrowUp = () => {
+    return (
+        <FontAwesomeIcon size='sm' icon={faArrowUp} />
+    );
+};
+
+export default ArrowUp;
---
+++ admin/src/components/Utils/Icons/ArrowsUpDown.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsUpDown } from '@fortawesome/free-solid-svg-icons';
+
+const ArrowsUpDown = () => {
+    return (
+        <FontAwesomeIcon size='sm' icon={faArrowsUpDown} />
+    );
+};
+
+export default ArrowsUpDown;
