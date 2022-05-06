cf3908a9621d35ddf5ff60abb8e7645e7470dccb
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-06 22:02:27 +0700

    add new hooks to support sorting

--- admin/src/hooks/index.ts
+++ admin/src/hooks/index.ts
@@ -1,5 +1,7 @@
-export { useDebouncedSearch } from './use-debounced-search';
-export { useDebouncedSearchParams } from './use-debounced-search-params';
-export { usePagination } from './use-pagination';
 export { useDateRangeSearchParams } from './use-date-range-search-params';
+export { useDebouncedSearchParams } from './use-debounced-search-params';
+export { useToggleSortParams } from './use-toggle-sort-params';
+export { useDebouncedSearch } from './use-debounced-search';
+export { useLoopToggle } from './use-loop-toggle';
+export { usePagination } from './use-pagination';
 export { useTabs } from './use-tabs';
