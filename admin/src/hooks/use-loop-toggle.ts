d1b53d554f957f6b12d1fc200f2c624a2d7aa294
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-08 20:33:14 +0700

    update import

--- admin/src/hooks/use-loop-toggle.ts
+++ admin/src/hooks/use-loop-toggle.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// unused
 export const useLoopToggle = (values: Array<string>, defaultValueIndex: number) => {
     const [current, setCurrent] = useState(values[defaultValueIndex] || values[0]);
 
