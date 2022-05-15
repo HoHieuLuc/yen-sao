3bd5417b5110de64c9593c88a15c8999e09a2dd9
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-15 16:19:50 +0700

    change findOne to exists

--- server/controllers/san-pham.controller.js
+++ server/controllers/san-pham.controller.js
@@ -23,9 +23,9 @@ const update = async (id, sanPhamData) => {
 const remove = async (id) => {
     try {
         // kiểm tra xem sản phẩm có phiếu nhập hay phiếu xuất không
-        const phieuNhaps = await ChiTietPhieuNhap.find({ maSanPham: id });
-        const phieuXuats = await ChiTietPhieuXuat.find({ maSanPham: id });
-        if (phieuNhaps.length > 0 || phieuXuats.length > 0) {
+        const phieuNhaps = await ChiTietPhieuNhap.exists({ maSanPham: id });
+        const phieuXuats = await ChiTietPhieuXuat.exists({ maSanPham: id });
+        if (phieuNhaps || phieuXuats) {
             throw new UserInputError('Bạn không thể xóa sản phẩm này');
         }
         return sanPhamService.remove(id);
