7a9fe47f02e0897bb6f0e3ecae803d16e14363fa
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-15 15:18:03 +0700

    only admin can upload

--- server/graphql/schema/file.js
+++ server/graphql/schema/file.js
@@ -1,7 +1,7 @@
 const { gql } = require('apollo-server-express');
 const { GraphQLUpload } = require('graphql-upload');
 const chainMiddlewares = require('../../middlewares/index');
-const authRequired = require('../../middlewares/authentication');
+const { adminRequired } = require('../../middlewares/authentication');
 const uploadController = require('../../controllers/upload.controller');
 
 const typeDefs = gql`
@@ -26,7 +26,7 @@ const typeDefs = gql`
 const resolvers = {
     Upload: GraphQLUpload,
     Mutation: {
-        upload: chainMiddlewares(authRequired,
+        upload: chainMiddlewares(adminRequired,
             () => ({})
         ),
     },
