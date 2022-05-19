9c01cff4fd306ed204d9f2200aff4cc0828fb46e
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-19 23:14:01 +0700

    fix typo

--- admin/src/components/Manage/User/Details/ActivityTab.tsx
+++ admin/src/components/Manage/User/Details/ActivityTab.tsx
@@ -2,7 +2,7 @@ import { usePagination } from '../../../../hooks';
 
 import LoadingWrapper from '../../../Utils/Wrappers/LoadingWrapper';
 import MyPagination from '../../../Utils/Pagination/MyPagination';
-import ActivityList from '../../Activity/List/ActivityList';
+import ActivityList from '../../../Activity/List/ActivityList';
 import ErrorPage from '../../../Utils/Errors/ErrorPage';
 import { Center, Stack, Title } from '@mantine/core';
 
@@ -14,7 +14,7 @@ interface Props {
 
 const ActivityTab = ({ id }: Props) => {
     const { currentPage, handlePageChange, limit, handleLimitChange } = usePagination();
-    const { data, loading, error } = activityHooks.useActivitesByUserId({
+    const { data, loading, error } = activityHooks.useActivitiesByUserId({
         page: currentPage,
         limit,
         userId: id
