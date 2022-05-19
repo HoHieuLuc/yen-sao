6486b3ec9bc589533cf9a75489bb44435aa9e072
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-19 21:38:24 +0700

    update PhieuNhapDetails components to be reuseable

--- admin/src/components/PhieuNhap/Details/PhieuNhapDetails.tsx
+++ admin/src/components/PhieuNhap/Details/PhieuNhapDetails.tsx
@@ -1,87 +1,43 @@
-import { useParams, Link, useNavigate } from 'react-router-dom';
-import { useDocumentTitle } from '@mantine/hooks';
-import { useModals } from '@mantine/modals';
-
-import { Box, Button, Center, Grid, Group, Title } from '@mantine/core';
-import LoadingWrapper from '../../Utils/Wrappers/LoadingWrapper';
 import ChiTietPhieuNhapItem from './ChiTietPhieuNhapItem';
-import DeletePhieuNhap from '../Delete/DeletePhieuNhap';
-import NotFound from '../../Utils/Errors/NotFound';
+import { Grid } from '@mantine/core';
 
 import { convertToShortDate, convertToVND } from '../../../utils/common';
-import { phieuNhapHooks } from '../../../graphql/queries';
 import { PhieuNhap } from '../../../types';
 
-const PhieuNhapDetails = () => {
-    const navigate = useNavigate();
-    const { id } = useParams();
-    const { data, loading, error } = phieuNhapHooks.usePhieuNhapByID(id || '');
-    useDocumentTitle(
-        data && data.phieuNhap.byID
-            ? `Phiếu nhập ngày ${convertToShortDate(data.phieuNhap.byID.ngayNhap)}`
-            : 'Đang tải...'
-    );
+interface Props {
+    data: PhieuNhap;
+}
 
-    const modals = useModals();
-
-    const openDeleteModal = (phieuNhap: PhieuNhap) => {
-        const modalId = modals.openModal({
-            title: <h3>Xóa phiếu nhập</h3>,
-            children: <DeletePhieuNhap
-                phieuNhap={phieuNhap}
-                closeModal={() => modals.closeModal(modalId)}
-                callback={() => navigate('/phieu-nhap')}
-            />
-        });
-    };
-
-    if (error || !id || (data && data.phieuNhap && data.phieuNhap.byID === null)) {
-        return <NotFound />;
-    }
-
+const PhieuNhapDetails = ({ data }: Props) => {
     return (
-        <LoadingWrapper loading={loading}>
-            {data && <Box>
-                <Center mb='md'>
-                    <Title>Chi tiết phiếu nhập</Title>
-                </Center>
-                <Grid gutter={10} mb='sm'>
-                    <Grid.Col span={4}>Ngày nhập:</Grid.Col>
-                    <Grid.Col span={8}>
-                        {convertToShortDate(data.phieuNhap.byID.ngayNhap)}
-                    </Grid.Col>
-                    <Grid.Col span={4}>Người nhập:</Grid.Col>
-                    <Grid.Col span={8}>
-                        {data.phieuNhap.byID.nguoiNhap.fullname}
-                    </Grid.Col>
-                    <Grid.Col span={4}>Số mặt hàng nhập:</Grid.Col>
-                    <Grid.Col span={8}>
-                        {data.phieuNhap.byID.soMatHangNhap}
-                    </Grid.Col>
-                    <Grid.Col span={4}>Tổng tiền:</Grid.Col>
-                    <Grid.Col span={8}>
-                        {convertToVND(data.phieuNhap.byID.tongTien)}
-                    </Grid.Col>
-                </Grid>
-
-                {data.phieuNhap.byID.chiTiet.map((item, index) => (
-                    <ChiTietPhieuNhapItem
-                        key={item.id}
-                        chiTietPhieuNhap={item}
-                        index={index}
-                    />
-                ))}
-
-                <Group position='right' mt='md'>
-                    <Button color='red' onClick={() => openDeleteModal(data.phieuNhap.byID)}>
-                        Xóa
-                    </Button>
-                    <Button color='teal' component={Link} to={`/phieu-nhap/${id}/sua`} >
-                        Sửa
-                    </Button>
-                </Group>
-            </Box>}
-        </LoadingWrapper>
+        <>
+            <Grid gutter={10} mb='sm'>
+                <Grid.Col span={4}>Ngày nhập:</Grid.Col>
+                <Grid.Col span={8}>
+                    {convertToShortDate(data.ngayNhap)}
+                </Grid.Col>
+                <Grid.Col span={4}>Người nhập:</Grid.Col>
+                <Grid.Col span={8}>
+                    {data.nguoiNhap.fullname}
+                </Grid.Col>
+                <Grid.Col span={4}>Số mặt hàng nhập:</Grid.Col>
+                <Grid.Col span={8}>
+                    {data.soMatHangNhap || data.chiTiet.length}
+                </Grid.Col>
+                <Grid.Col span={4}>Tổng tiền:</Grid.Col>
+                <Grid.Col span={8}>
+                    {convertToVND(data.tongTien)}
+                </Grid.Col>
+            </Grid>
+            
+            {data.chiTiet.map((item, index) => (
+                <ChiTietPhieuNhapItem
+                    key={item.id}
+                    chiTietPhieuNhap={item}
+                    index={index}
+                />
+            ))}
+        </>
     );
 };
 
