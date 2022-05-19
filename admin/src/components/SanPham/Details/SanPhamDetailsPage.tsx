2cb5d6425e3ccf9f9e555a2a65c3b355f1e6da49
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-19 23:15:42 +0700

    add activities tab for san pham details

--- admin/src/components/SanPham/Details/SanPhamDetailsPage.tsx
+++ admin/src/components/SanPham/Details/SanPhamDetailsPage.tsx
@@ -4,6 +4,7 @@ import { useTabs } from '../../../hooks';
 
 import ChiTietPhieuNhapList from './Tabs/ChiTietPhieuNhapList';
 import ChiTietPhieuXuatList from './Tabs/ChiTietPhieuXuatList';
+import ActivityTab from '../../Activity/Tab/ActivityTab';
 import NotFound from '../../Utils/Errors/NotFound';
 import DetailsTab from './Tabs/DetailsTab';
 import { Tabs } from '@mantine/core';
@@ -13,8 +14,8 @@ import { sanPhamHooks } from '../../../graphql/queries';
 const SanPhamDetailsPage = () => {
     const { id } = useParams();
     const { activeTab, onTabChange, currentTabTitle } = useTabs(
-        ['chi-tiet', 'nhap-hang', 'xuat-hang'],
-        ['Chi tiết', 'Thông tin nhập hàng', 'Thông tin xuất hàng']
+        ['chi-tiet', 'nhap-hang', 'xuat-hang', 'lich-su'],
+        ['Chi tiết', 'Thông tin nhập hàng', 'Thông tin xuất hàng', 'Lịch sử chỉnh sửa']
     );
     const { data, loading, error } = sanPhamHooks.useSanPhamByID(id || '');
 
@@ -55,6 +56,9 @@ const SanPhamDetailsPage = () => {
             </Tabs.Tab>
             <Tabs.Tab label="Thông tin xuất hàng" tabKey='xuat-hang'>
                 <ChiTietPhieuXuatList id={id} />
+            </Tabs.Tab>
+            <Tabs.Tab label="Lịch sử chỉnh sửa" tabKey='lich-su'>
+                <ActivityTab id={id} />
             </Tabs.Tab>
         </Tabs>
     );
