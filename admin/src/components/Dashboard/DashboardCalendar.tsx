fdf5b388f07400d5b778ddcbf9c47d0ca061629c
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-20 17:56:42 +0700

    update dashboard page

--- admin/src/components/Dashboard/DashboardCalendar.tsx
+++ admin/src/components/Dashboard/DashboardCalendar.tsx
@@ -1,37 +1,51 @@
 import { convertToShortDate } from '../../utils/common';
 import { Indicator } from '@mantine/core';
 import { Calendar } from '@mantine/dates';
+import { ChiTietPhieuNhap, ChiTietPhieuXuat } from '../../types';
 
-interface Props {
+interface NhapHang extends Base {
+    type: 'NhapHang';
+    data: Array<ChiTietPhieuNhap>;
+}
+
+interface XuatHang extends Base {
+    type: 'XuatHang';
+    data: Array<ChiTietPhieuXuat>;
+}
+
+interface Base {
+    type: 'NhapHang' | 'XuatHang';
+    data: Array<unknown>;
     selectedDay: Date | null;
     setSelectedDay: (day: Date | null) => void;
     month: Date;
     onMonthChange: (month: Date) => void;
-    data: Array<{
-        createdAt: number;
-        ngayNhap?: number;
-        ngayXuat?: number;
-    }>
 }
 
-const DashboardCalendar = ({ selectedDay, setSelectedDay, month, onMonthChange, data }: Props) => {
+type Props = NhapHang | XuatHang;
+
+const DashboardCalendar = ({ selectedDay, setSelectedDay, month, onMonthChange, data, type }: Props) => {
     return (
         <Calendar
             value={selectedDay}
             onChange={setSelectedDay}
             month={month}
             onMonthChange={onMonthChange}
+            hideOutsideDates
             renderDay={(date) => {
                 const day = date.getDate();
 
-                const dataToday = data.filter(item => {
-                    if (item.ngayNhap) {
+                let dataToday: Array<ChiTietPhieuNhap | ChiTietPhieuXuat> = [];
+
+                if (type === 'NhapHang') {
+                    dataToday = data.filter(item => {
                         return convertToShortDate(item.ngayNhap) === convertToShortDate(date);
-                    }
-                    if (item.ngayXuat) {
+                    });
+                } else {
+                    dataToday = data.filter(item => {
                         return convertToShortDate(item.ngayXuat) === convertToShortDate(date);
-                    }
-                });
+                    });
+                }
 
                 if (dataToday.length > 0) {
                     return <Indicator
--- admin/src/components/Dashboard/ThongTinNhapHang.tsx
+++ admin/src/components/Dashboard/ThongTinNhapHang.tsx
@@ -4,24 +4,28 @@ import { Anchor, Box, Grid, Paper, Stack, Text } from '@mantine/core';
 import LoadingWrapper from '../Utils/Wrappers/LoadingWrapper';
 import DashboardCalendar from './DashboardCalendar';
 import ErrorPage from '../Utils/Errors/ErrorPage';
+import { Calendar } from '@mantine/dates';
 import { Link } from 'react-router-dom';
 
 import { convertToShortDate, convertToVND } from '../../utils/common';
-import { phieuNhapHooks } from '../../graphql/queries';
-import { Calendar } from '@mantine/dates';
+import { chiTietPhieuNhapHooks } from '../../graphql/queries';
 import dayjs from 'dayjs';
-
 
 const ThongTinNhapHang = () => {
     const [month, onMonthChange] = useState(new Date());
     const [selectedDay, setSelectedDay] = useState<Date | null>(null);
 
-    const { data, loading, error } = phieuNhapHooks.useAllPhieuNhap(
+    const { data, loading, error } = chiTietPhieuNhapHooks.useAllChiTietPhieuNhaps(
         {
-            page: 1,
-            limit: 1000,
-            from: dayjs(month).startOf('month').toDate().getTime(),
-            to: dayjs(month).endOf('month').toDate().getTime()
+            from: dayjs(month).startOf('month').toDate(),
+            to: dayjs(month).endOf('month').toDate()
+        }
+    );
+
+    const chiTietPhieuNhapsOnSelectedDay = selectedDay && data?.chiTietPhieuNhap.all.filter(
+        item => {
+            const shortDate = convertToShortDate(item.ngayNhap);
+            return shortDate === convertToShortDate(selectedDay.getTime());
         }
     );
 
@@ -31,7 +35,7 @@ const ThongTinNhapHang = () => {
 
     return (
         <LoadingWrapper loading={loading}>
-            {loading && <Calendar initialMonth={month} />}
+            {loading && <Calendar hideOutsideDates initialMonth={month} />}
             {!loading && data && <Grid gutter='xs'>
                 <Grid.Col md={4}>
                     <DashboardCalendar
@@ -39,7 +43,8 @@ const ThongTinNhapHang = () => {
                         setSelectedDay={setSelectedDay}
                         onMonthChange={onMonthChange}
                         month={month}
-                        data={data.phieuNhap.all.docs}
+                        data={data.chiTietPhieuNhap.all}
+                        type='NhapHang'
                     />
                 </Grid.Col>
                 <Grid.Col md={8}>
@@ -49,24 +54,25 @@ const ThongTinNhapHang = () => {
                         </Text>
                         <Text>
                             Tổng tiền nhập hàng: {convertToVND(
-                                data.phieuNhap.all.docs.reduce(
-                                    (sum, curr) => sum + curr.tongTien, 0)
+                                data.chiTietPhieuNhap.all.reduce(
+                                    (sum, curr) => sum + curr.thanhTien, 0)
                             )}
                         </Text>
                     </Stack>
                     <Paper shadow="xl" p="sm" withBorder>
-                        {selectedDay
-                            ? data.phieuNhap.all.docs.filter(
-                                item => {
-                                    const shortDate = convertToShortDate(item.ngayNhap);
-                                    return shortDate === convertToShortDate(selectedDay.getTime());
-                                }
-                            ).map(item => {
+                        {chiTietPhieuNhapsOnSelectedDay && chiTietPhieuNhapsOnSelectedDay.length > 0
+                            ? chiTietPhieuNhapsOnSelectedDay.map(item => {
                                 return <Box key={item.id}>
                                     <Anchor component={Link} to={`/phieu-nhap/${item.id}`}>
                                         {convertToShortDate(item.ngayNhap)}
-                                    </Anchor>: nhập {item.soMatHangNhap} mặt hàng, tổng tiền {
-                                        convertToVND(item.tongTien)
+                                    </Anchor>
+                                    : nhập {item.soLuongNhap / 1000} kg
+                                    {' '}
+                                    <Anchor component={Link} to={`/san-pham/${item.sanPham.id}`}>
+                                        {item.sanPham.tenSanPham}
+                                    </Anchor>
+                                    , thành tiền {
+                                        convertToVND(item.thanhTien)
                                     }
                                 </Box>;
                             })
