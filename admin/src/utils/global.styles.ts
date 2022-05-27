562d30e787f9210476fa793ab343f0305f31f7c9
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-27 17:39:05 +0700

    update rte style

--- admin/src/utils/global.styles.ts
+++ admin/src/utils/global.styles.ts
@@ -23,12 +23,9 @@ export default createStyles(
                     marginLeft: 'auto',
                 }
             },
-            p: {
-                margin: '0'
+            '& p, & img, & h1, & h2, & h3, & h4, & h5, & h6': {
+                margin: 0
             },
-            img: {
-                margin: '0'
-            }
         }
     })
 );
