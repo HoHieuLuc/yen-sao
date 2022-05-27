8395ac66ef717e7175755df0d23781d5de7833bc
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-27 16:12:42 +0700

    add SanPhamMultiSelect

---
+++ admin/src/components/SanPham/Form/SanPhamMultiSelect.tsx
@@ -0,0 +1,54 @@
+import { useQuery, gql } from '@apollo/client';
+
+import { Loader, MultiSelect } from '@mantine/core';
+import { GetInputPropsPayload } from '@mantine/form/lib/types';
+import { AllSanPhams, SearchSanPhamVars } from '../../../types';
+
+type Props = GetInputPropsPayload & {
+    label: string;
+    description: string;
+    placeholder: string;
+};
+
+const SanPhamMultiSelect = (props: Props) => {
+    const { data, loading } = useQuery<AllSanPhams, SearchSanPhamVars>(
+        gql`
+            query AllSanPhams($page: Int!, $limit: Int!, $search: String) {
+                sanPham {
+                    all(page: $page, limit: $limit, search: $search) {
+                        docs {
+                            id
+                            tenSanPham
+                        }
+                    }
+                }
+            }
+        `,
+        {
+            variables: {
+                page: 1,
+                limit: 100000,
+                search: ''
+            }
+        }
+    );
+
+    const sanPhamData = data ? data.sanPham.all.docs.map(
+        (sanPham) => ({
+            value: sanPham.id,
+            label: sanPham.tenSanPham,
+        })
+    ) : [];
+
+    return (
+        <MultiSelect
+            nothingFound={loading ? <Loader /> : 'Không tìm thấy sản phẩm nào'}
+            data={sanPhamData}
+            {...props}
+            searchable
+            clearable
+        />
+    );
+};
+
+export default SanPhamMultiSelect;
