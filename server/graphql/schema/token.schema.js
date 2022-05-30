c4b11a5a345146313b3c83710625ef5e84793b7f
Ho Hieu Luc <kingofika123@gmail.com>
2022-05-30 17:39:52 +0700

    add thong ke schema (wip)

--- server/graphql/schema/token.schema.js
+++ server/graphql/schema/token.schema.js
@@ -1,42 +1,11 @@
 const { gql } = require('apollo-server');
-const PhieuNhap = require('../../models/PhieuNhap');
 
 const typeDefs = gql`
     type Token {
         value: String!
     }
-
-    type ThongKe {
-        value: Object
-    }
-
-    extend type Query {
-        "experimental"
-        thongKe: ThongKe
-    }
 `;
 
-const resolvers = {
-    Query: {
-        thongKe: async () => {
-            const value = await PhieuNhap.aggregate([
-                {
-                    $group: {
-                        _id: {
-                            month: { $month: '$ngayNhap' },
-                            year: { $year: '$ngayNhap' },
-                        },
-                        total: { $sum: '$tongTien' },
-                        ngayNhap: { $first: '$ngayNhap' },
-                    }
-                }
-            ]);
-            return { value };
-        }
-    }
-};
-
 module.exports = {
-    typeDefs,
-    resolvers
+    typeDefs
 };
