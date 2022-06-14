9f3028363c0915a81deb03b38cdba6464061eaba
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-15 00:40:53 +0700

    add CreateCamNang component

---
+++ admin/src/components/CamNang/Create/CreateCamNang.tsx
@@ -0,0 +1,42 @@
+import { useDocumentTitle } from '@mantine/hooks';
+
+import { Center, Stack, Title } from '@mantine/core';
+import CamNangForm from '../Form/CamNangForm';
+
+import { showErrorNotification } from '../../../events';
+import { camNangHooks } from '../../../graphql/queries';
+import { CamNangFormData } from '../../../types';
+
+interface Props {
+    title: string;
+}
+
+const CreateCamNang = ({ title }: Props) => {
+    useDocumentTitle(title);
+    const [createCamNang, { loading }] = camNangHooks.useCreateCamNang();
+
+    const handleCreateCamNang = (values: CamNangFormData) => {
+        if (!values.anhDaiDien) {
+            return showErrorNotification('Vui lòng chọn ít nhất 1 ảnh');
+        }
+        void createCamNang({
+            variables: {
+                payload: values
+            }
+        });
+    };
+
+    return (
+        <Stack spacing='xs'>
+            <Center>
+                <Title>Thêm cẩm nang mới</Title>
+            </Center>
+            <CamNangForm
+                loading={loading}
+                onSubmit={handleCreateCamNang}
+            />
+        </Stack>
+    );
+};
+
+export default CreateCamNang;
