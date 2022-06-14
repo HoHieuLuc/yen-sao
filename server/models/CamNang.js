705b810ccc67dabc050298e5d16fe695677c8cee
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-15 00:25:33 +0700

    add CamNang model

---
+++ server/models/CamNang.js
@@ -0,0 +1,77 @@
+const mongoosePaginate = require('mongoose-paginate-v2');
+const sanitizeHtml = require('sanitize-html');
+const mongoose = require('mongoose');
+const { nanoid } = require('nanoid');
+const slugify = require('slugify');
+
+const camNangSchema = mongoose.Schema(
+    {
+        tieuDe: {
+            type: String,
+            required: true
+        },
+        noiDung: {
+            type: String,
+            required: true
+        },
+        isPublic: {
+            type: Boolean,
+            required: true
+        },
+        slug: {
+            type: String,
+        },
+        anhDaiDien: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+camNangSchema.pre('save', async function () {
+    const slug = slugify(
+        this.tieuDe,
+        {
+            lower: true,
+            locale: 'vi'
+        }
+    );
+    this.slug = `${slug}-${nanoid(5)}`;
+    this.noiDung = sanitizeHtml(this.noiDung, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        allowedClasses: {
+            '*': ['*']
+        }
+    });
+});
+
+camNangSchema.pre('findOneAndUpdate', async function (next) {
+    this.getUpdate().noiDung = sanitizeHtml(this.getUpdate().noiDung, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        allowedClasses: {
+            '*': ['*']
+        }
+    });
+
+    const camNangToUpdate = await this.model.findById(this.getQuery()._id);
+
+    if (!this.getUpdate().tieuDe || camNangToUpdate.tieuDe === this.getUpdate().tieuDe) {
+        return next();
+    }
+
+    const slug = slugify(
+        this._update.tieuDe,
+        {
+            lower: true,
+            locale: 'vi'
+        }
+    );
+    this.getUpdate().slug = `${slug}-${nanoid(5)}`;
+});
+
+camNangSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model('CamNang', camNangSchema);
