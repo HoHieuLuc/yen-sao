6541d2d07aee3e32e1cfb25b6b3e61aa72d0c2ef
Ho Hieu Luc <kingofika123@gmail.com>
2022-06-19 17:51:09 +0700

    chore(admin): update ErrorPage

--- admin/src/components/Utils/Errors/ErrorPage.tsx
+++ admin/src/components/Utils/Errors/ErrorPage.tsx
@@ -52,7 +52,7 @@ const ErrorPage = () => {
                 <div className={classes.label}>500</div>
                 <Title className={classes.title}>Có lỗi xảy ra</Title>
                 <Text size='lg' align='center' className={classes.description}>
-                    Máy chủ không thể xử lý yêu cầu của bạn
+                    Máy chủ không thể xử lý yêu cầu của bạn, hãy thử tải lại trang
                 </Text>
                 <Group position='center'>
                     <Button component={Link} to='/' variant='subtle' size='md'>
